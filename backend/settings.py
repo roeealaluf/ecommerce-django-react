57bc447f1974c044c5839c7601edd6e6a7b6607d
roeealaluf <162418635+roeealaluf@users.noreply.github.com>
2024-07-07 14:22:53 +0300

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -188,3 +188,4 @@ CORS_ALLOW_ALL_ORIGINS = True
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+RUN_SERVER_PORT=80
